f9c9be3d17e6c314058a4600f1fef303b4a54f91
Dane <daners88@yahoo.com>
2020-02-15 22:52:13 -0700

    adding to source control

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    System.Random rand = new System.Random();
+
+    public bool running = false;
+
+    public static GameManager Instance;
+
+    private void Awake()
+    {
+        if (Instance == null)
+            Instance = this;
+
+        else if (Instance != this)
+            Destroy(gameObject);
+
+        //DontDestroyOnLoad(gameObject);
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (running)
+        {
+
+        }
+        else
+        {
+  
+        }
+    }
+
+
+}
